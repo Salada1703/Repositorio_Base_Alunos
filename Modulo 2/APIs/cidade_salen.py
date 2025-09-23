7043dd7967069dec1dd61e611d30ea7208a78788
Salada1703 <gferreira170310@gmail.com>
2025-09-23 11:47:48 -0300

    aula de API pagina web
    filmes html

---
+++ Modulo 2/APIs/cidade_salen.py
@@ -0,0 +1,19 @@
+import requests
+
+#response= requests.get('https://www.instagram.com')
+
+#print(response.status_code)
+#print(response.text)
+print("------z-----")
+
+import requests
+
+usuario={
+    "name":"salada do 7",
+}
+#response=requests.post('https://68d00787ec1a5ff338263f44.mockapi.io/cidade/Cidadaos',json=usuario)
+#response=requests.delete('https://68d00787ec1a5ff338263f44.mockapi.io/cidade/Cidadaos/13',json=usuario)
+
+
+print(response.status_code)
+print(response.json())

a8c7773b9654fe6d05b3969666361f2a68031b27
Salada1703 <gferreira170310@gmail.com>
2025-09-25 11:51:47 -0300

    Update cidade_salen.py

--- Modulo 2/APIs/cidade_salen.py
+++ Modulo 2/APIs/cidade_salen.py
@@ -9,10 +9,10 @@ print("------z-----")
 import requests
 
 usuario={
-    "name":"salada do 7",
+    "name":"emrehliug",
 }
 #response=requests.post('https://68d00787ec1a5ff338263f44.mockapi.io/cidade/Cidadaos',json=usuario)
-#response=requests.delete('https://68d00787ec1a5ff338263f44.mockapi.io/cidade/Cidadaos/13',json=usuario)
+response=requests.delete('https://68d00787ec1a5ff338263f44.mockapi.io/cidade/Cidadaos/1',json=usuario)
 
 
 print(response.status_code)
